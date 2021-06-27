b6aa4cecb1516bd905b75298ee97aae843f4f976
Jahadul-Rakib <rakibdiu2015@gmail.com>
2021-06-27 21:34:41 +0600

    add

--- synch_wait/single_group.go
+++ synch_wait/single_group.go
@@ -1,6 +1,9 @@
 package main
 
-import "sync"
+import (
+	"log"
+	"sync"
+)
 
 func main() {
 	var number int
@@ -8,9 +11,17 @@ func main() {
 	var wg sync.WaitGroup
 
 	wg.Add(1)
-	go func(){
+	go func() {
 		wg.Done()
-		number ++
+		number++
+		log.Println(number)
+	}()
+
+	wg.Add(1)
+	go func() {
+		wg.Done()
+		number++
+		log.Println(number)
 	}()
 	wg.Wait()
 	println("Data value is: ", number)
